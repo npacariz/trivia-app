072229f61817f55233715b0e699ca6a3d0dda4dc
npacariz <nikolapacariz@gmail.com>
2018-08-24 18:13:35 +0200

    Zadatak 1

---
+++ src/components/NavBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="NavBar">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <router-link class="navbar-brand" to="/">Trivia App</router-link>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+        <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-item nav-link" href="#">Features</a>
+        <a class="nav-item nav-link" href="#">Pricing</a>
+        <a class="nav-item nav-link disabled" href="#">Disabled</a>
+    </div>
+  </div>
+</nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+
+}
+</script>
+<style scoped>
+
+</style>
---
+++ src/pages/TriviaApp.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="TriviaApp">
+    <h1>Trivia App</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TriviaApp',
+
+}
+</script>
+<style scoped>
+
+</style>
